129a44915774fcc52b7efbb254a3beee1f0a5a7f
Ryan <mayue3434@gmail.com>
2017-02-05 22:25:54 -0800

    fix bugs and add a logout button

--- Product Sales/Site2.Master.cs
+++ Product Sales/Site2.Master.cs
@@ -10,9 +10,11 @@ namespace Product_Sales
     public partial class Site2 : System.Web.UI.MasterPage
     {
         protected String url;
+        protected String category;
         protected void Page_Load(object sender, EventArgs e)
         {
             url = Request.Url.LocalPath;
+            category = Request.QueryString["Category"];
         }
     }
 }
